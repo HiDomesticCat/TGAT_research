1a01d5daefeabeef66b3aeae0b4b9039cbdc4379
HiCat <hiorangecat12888@gmail.com>
2025-04-27 03:45:57 +0800

    Add track_memory_usage to utils/__init__.py exports

--- tgat_network_ids/src/utils/__init__.py
+++ tgat_network_ids/src/utils/__init__.py
@@ -1,10 +1,11 @@
 from .memory_utils import (
     clean_memory, memory_usage_decorator, print_memory_usage,
-    get_memory_usage, print_optimization_suggestions, MemoryMonitor
+    get_memory_usage, print_optimization_suggestions, MemoryMonitor,
+    track_memory_usage
 )
 
 from .utils import (
-    set_seed, get_device, load_config, save_config, 
-    evaluate_predictions, format_metrics, create_dir, 
+    set_seed, get_device, load_config, save_config,
+    evaluate_predictions, format_metrics, create_dir,
     save_results, get_timestamp, time_execution
 )
