1ded481cdf86311ec2e58a32497c3f6d92e58ec4
HiCat <hiorangecat12888@gmail.com>
2025-04-27 03:41:05 +0800

    Fix time encoding import in models/__init__.py using alias

--- tgat_network_ids/src/models/__init__.py
+++ tgat_network_ids/src/models/__init__.py
@@ -1,4 +1,4 @@
 from .tgat_model import TGAT, TemporalGATLayer, TimeEncoding
 from .optimized_tgat_model import OptimizedTGATModel
 from .memory_optimized_train import MemoryOptimizedTGATTrainer
-from .time_encoding import AdvancedTimeEncoding
+from .time_encoding import MemoryEfficientTimeEncoding as AdvancedTimeEncoding
